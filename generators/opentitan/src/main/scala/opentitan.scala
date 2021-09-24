5bb712c08be79c01bfd04cacadf8b062816921d7
Brendon Chetwynd <brendon.chetwynd@ll.mit.edu>
2021-09-24 08:39:41 -0400

    - Added opentitan submodule initialization to chipyard script

--- generators/opentitan/src/main/scala/opentitan.scala
+++ generators/opentitan/src/main/scala/opentitan.scala
@@ -3,7 +3,7 @@ package opentitan.resources
 import chisel3._
 import chisel3.util._
 
-// Add all the LLKI SystemVerilog Modules (or customized OpenTitan modules)
+// Add the non-customized OpenTitan modules
 addResource("/vsrc/prim_rtl/prim_assert.sv")
 addResource("/vsrc/prim_rtl/prim_util_pkg.sv")
 addResource("/vsrc/prim_rtl/prim_fifo_sync.sv")
