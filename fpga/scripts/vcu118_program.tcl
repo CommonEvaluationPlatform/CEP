d6534d1939c6afeedf7e944dfacc902187488c9c
Brendon Chetwynd <brendon.chetwynd@ll.mit.edu>
2021-09-30 12:18:14 -0400

    - Minor makefile / script cleanup

--- fpga/scripts/vcu118_program.tcl
+++ fpga/scripts/vcu118_program.tcl
@@ -1,4 +1,4 @@
-# Script to program the VC-118 memory configuration device
+# Script to program the VCU-118 memory configuration device
 open_hw
 
 # check the number of parameters
@@ -11,16 +11,16 @@ if {$argc < 1 || $argc > 2} {
 
 lassign $argv mcsfile remote_ip
 
-# program to a locally connected board
+# Connected to a Local Board
 if {$argc == 1} {
 	connect_hw_server
+# Connect to a remote board
 } elseif {$argc == 2} {
 	connect_hw_server -url $remote_ip
 }
 
+# Connect to the hardware target
 open_hw_target
-
-
 
 # Add the memory configuration device
 create_hw_cfgmem -hw_device [lindex [get_hw_devices xcvu9p_0] 0] [lindex [get_cfgmem_parts {mt25qu01g-spi-x1_x2_x4_x8}] 0]
@@ -31,6 +31,7 @@ set_property PROGRAM.VERIFY  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_
 set_property PROGRAM.CHECKSUM  0 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xcvu9p_0] 0]]
 refresh_hw_device [lindex [get_hw_devices xcvu9p_0] 0]
 
+# Specify the programming parameters
 set_property PROGRAM.ADDRESS_RANGE  {use_file} [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xcvu9p_0] 0]]
 set_property PROGRAM.FILES [list ${mcsfile}_primary.mcs ${mcsfile}_secondary.mcs] [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xcvu9p_0] 0]]
 set_property PROGRAM.PRM_FILES [list ${mcsfile}_primary.prm ${mcsfile}_secondary.prm] [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xcvu9p_0] 0]]
@@ -41,11 +42,13 @@ set_property PROGRAM.CFG_PROGRAM  1 [ get_property PROGRAM.HW_CFGMEM [lindex [ge
 set_property PROGRAM.VERIFY  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xcvu9p_0] 0]]
 set_property PROGRAM.CHECKSUM  0 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xcvu9p_0] 0]]
 
+# Program the memory devices
 startgroup 
 create_hw_bitstream -hw_device [lindex [get_hw_devices xcvu9p_0] 0] [get_property PROGRAM.HW_CFGMEM_BITFILE [ lindex [get_hw_devices xcvu9p_0] 0]]; program_hw_devices [lindex [get_hw_devices xcvu9p_0] 0]; refresh_hw_device [lindex [get_hw_devices xcvu9p_0] 0];
 program_hw_cfgmem -hw_cfgmem [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xcvu9p_0] 0]]
 endgroup
 
+# Close things out
 close_hw
 quit
 
