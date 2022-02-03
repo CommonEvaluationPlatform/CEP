0d0fb00a7a763e46fd2ac3608cc6234ed7fd4dd8
Brendon Chetwynd <brendon.chetwynd@ll.mit.edu>
2022-02-03 08:30:45 -0500

    - miscTests passes both bareMetal and bfmMode

--- sims/cep_cosim/testSuites/bareMetalTests/miscTests/riscv_wrapper.cc
+++ sims/cep_cosim/testSuites/bareMetalTests/miscTests/riscv_wrapper.cc
@@ -1,59 +1,58 @@
-//************************************************************************
+//--------------------------------------------------------------------------------------
 // Copyright 2021 Massachusetts Institute of Technology
 // SPDX short identifier: BSD-2-Clause
 //
-// File Name:      
+// File Name:      riscv_wrapper.cc
 // Program:        Common Evaluation Platform (CEP)
 // Description:    
 // Notes:          
 //
-//************************************************************************
-//
+//--------------------------------------------------------------------------------------
+
 // For bareMetal mode ONLY
-//
 #ifdef BARE_MODE
-#include <stdlib.h>
-#include "cep_adrMap.h"
-#include "cep_apis.h"
-#include "portable_io.h"
-#include "cepUartTest.h"
-#include "cepSpiTest.h"
-#include "cepGpioTest.h"
-#include "cepMaskromTest.h"
+  #include "cep_apis.h"
+  #include "cepregression.h"
+  #include "CEP.h"
+  
+  #include "cepUartTest.h"
+  #include "cepSpiTest.h"
+  #include "cepGpioTest.h"
+  #include "cepMaskromTest.h"
+  
+  #ifdef __cplusplus
+  extern "C" {
+  #endif
+  
+  void thread_entry(int cid, int nc) {
+    
+    int errCnt    = 0;
+    int testId[4] = {0x00, 0x11, 0x22, 0x33};
+    int coreId    = read_csr(mhartid);
+    int revCheck  = 1;
+    int verbose   = 0;
+    
+    set_printf(0);
+  
+    // Set the current core's status to running
+    set_cur_status(CEP_RUNNING_STATUS);
+  
+    switch (coreId) {
+      case 0 :  errCnt += cepUartTest_runTest(coreId,0x10, 0); break;
+      case 1 :  errCnt += cepSpiTest_runTest(coreId,0x20, 0); break;
+      case 2 :  errCnt += cepGpioTest_runTest(coreId,0x30,0); break;
+      case 3 :  errCnt += cepMaskromTest_runTest(coreId,0x40,0); break;
+    }
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+    // Set the core status
+    set_status(errCnt, testId[coreId]);
 
-#define flush_page(addr) asm volatile ("sfence.vma %0" : : "r" (addr) : "memory")
-
-//int main(void)
-void thread_entry(int cid, int nc)
-{
-  int errCnt = 0;
-  int stat = 0;
-  int cpuId = read_csr(mhartid);
-  set_printf(0);
-
-  switch (cpuId) {
-  case 3 :  errCnt += cepUartTest_runTest(cpuId,0x10, 0); break;
-  case 1 :  errCnt += cepSpiTest_runTest(cpuId,0x20, 0); break;
-  case 2 :  errCnt += cepGpioTest_runTest(cpuId,0x30,0); break;
-  case 0 :  errCnt += cepMaskromTest_runTest(cpuId,0x40,0); break;
+    // Exit with the error count
+    exit(errCnt);
   }
 
-  //
-  // Done
-  //
-  set_status(errCnt,errCnt);
-  //
-  // Stuck here forever...
-  //
-  exit(errCnt);
-}
-
-#ifdef __cplusplus
-}
-#endif
+  #ifdef __cplusplus
+  }
+  #endif
   
-#endif
+#endif // #ifdef BARE_MODE
