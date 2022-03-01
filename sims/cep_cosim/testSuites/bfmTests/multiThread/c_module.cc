22fb77de4cbbf47eda7c5db624c0a92352a75d0d
mblood <Maria.Blood@ll.mit.edu>
2022-03-01 11:00:49 -0500

    Updates to multiThread to work with Chipyard

--- sims/cep_cosim/testSuites/bfmTests/multiThread/c_module.cc
+++ sims/cep_cosim/testSuites/bfmTests/multiThread/c_module.cc
@@ -1,33 +1,30 @@
-//************************************************************************
-// Copyright 2022 Massachusets Institute of Technology
+//--------------------------------------------------------------------------------------
+// Copyright 2021 Massachusetts Institute of Technology
 // SPDX License Identifier: BSD-2-Clause
 //
-// File Name:      
+// File Name:      c_module.cc
 // Program:        Common Evaluation Platform (CEP)
 // Description:    
 // Notes:          
 //
-//************************************************************************
+//--------------------------------------------------------------------------------------
 #include "v2c_cmds.h"
 #include "simPio.h"
-//
 #include "shMem.h"
 #include "c_module.h"
 #include <unistd.h>
 #include "random48.h"
-
-#include "CEP.h"
+#include "CEP.h" 
 #include "cep_apis.h"
 #include "simdiag_global.h"
 #include "cepMultiThread.h"
 
-//
+
 void *c_module(void *arg) {
 
-
-  // ======================================
-  // Set up
-  // ======================================
+  //--------------------------------------------------------------------------------------
+  // Test Set up
+  //--------------------------------------------------------------------------------------
   pthread_parm_t *tParm = (pthread_parm_t *)arg;
   int errCnt = 0;
   int slotId = tParm->slotId;
@@ -35,54 +32,53 @@ void *c_module(void *arg) {
   int verbose = tParm->verbose;
   Int32U seed = tParm->seed;
   int restart = tParm->restart;
-  int offset = GET_OFFSET(slotId,cpuId);
-  GlobalShMemory.getSlotCpuId(offset,&slotId,&cpuId);
+  int offset = GET_OFFSET(slotId, cpuId);
+  GlobalShMemory.getSlotCpuId(offset, &slotId, &cpuId);
+
   shIpc *ptr = GlobalShMemory.getIpcPtr(offset);
   ptr->SetAliveStatus();
   sleep(1);
 
-  // ======================================
-  // Test is Here
-  // ======================================
   simPio pio;
-  pio.MaybeAThread(); // chec
+  pio.MaybeAThread(); 
   pio.EnableShIpc(1);
   pio.SetVerbose(verbose);
 
-  //
-  // ======================================
+  //--------------------------------------------------------------------------------------
   // Test starts here
-  // ======================================
-  // MUST
-  // wait until Calibration is done..
-  int calibDone = calibrate_ddr3(50);
-      //pio.RunClk(1000);
+  //--------------------------------------------------------------------------------------
+ 
+  // Wait until Calibration is done...
+  pio.RunClk(500);
 
-  int cpuActiveMask = seed; // seed is used as cpuActiveMask from c_displatch
+  // Indicate which cores to enable 
+  // (all cores minus GPS because not currently working in MultiThread)
+  //int coreMask = 0xFFFFCFFB; 
+  int coreMask = 0xFFFF0FFF;
 
-  int maxTest = 10;
+  //int maxTest = CEP_LLKI_CORES;
+  int maxTest = 16;
   int maxLoop = 2;
-  uint64_t testLockBuf = 0x90000000;
+ 
+  uint64_t testLockBuf = CEPREGS_BASE_ADDR + cep_scratch0_reg;
   
-  if (!errCnt) {
-      errCnt += cepMultiThread_setup(cpuId, testLockBuf, maxTest, cpuActiveMask, verbose) ;
-  }
-  //    LOGI(" errCnt=%d\n",errCnt);  
-  if (!errCnt) { errCnt += cepMultiThread_runThr(cpuId, testLockBuf, cpuActiveMask, maxTest, maxLoop,seed, verbose);  }
-      //
-  //
+  // Setup SRoT and initialize scratchpad space for test status
+  errCnt += cepMultiThread_setup(cpuId, testLockBuf, coreMask, maxTest, GlobalShMemory.getActiveMask(), verbose);
+  
+  // Run cepMultiThread 
+  errCnt += cepMultiThread_runThr(cpuId, testLockBuf, coreMask, maxTest, maxLoop, seed, verbose);
+      
   pio.RunClk(100);  
-  //
-  // ======================================
+
+  //--------------------------------------------------------------------------------------
   // Exit
-  // ======================================
-cleanup:
+  //--------------------------------------------------------------------------------------
+ cleanup:
   if (errCnt != 0) {
     LOGI("======== TEST FAIL ========== %x\n",errCnt);
   } else {
     LOGI("======== TEST PASS ========== \n");    
   }
-  //  shIpc *ptr = GlobalShMemory.getIpcPtr(offset);
   ptr->SetError(errCnt);
   ptr->SetThreadDone();
   pthread_exit(NULL);
