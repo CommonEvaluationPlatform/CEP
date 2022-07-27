4ade1bf3a3aa53930babd6665ac5c09a3fb75dca
Brendon Chetwynd <brendon.chetwynd@ll.mit.edu>
2022-07-27 14:40:55 -0400

    - Yet another typo in linux/gpiotest.c

--- software/linux/gpiotest/gpiotest.c
+++ software/linux/gpiotest/gpiotest.c
@@ -131,8 +131,8 @@ int main(int argc, char **argv)
     for (i = 0; i < GPIO_WIDTH; i++) {
       if (!compare_arrays(values_new, values_old, GPIO_WIDTH)) {
         printf("switches = ");
-        for (j = GPIO_WIDTH - 1; j >= 0; j--) {
-          printf("%0x", values_new[j]);
+        for (j = 0; j < GPIO_WIDTH; j++) {
+          printf("%0x", values_new[GPIO_WIDTH - j - 1]);
           values_old[j] = values_new[j];
         }
         printf("\n");
