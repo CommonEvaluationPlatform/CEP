4873dda69fdb940aca6e832bdb335d3fed273416
Brendon Chetwynd <brendon.chetwynd@ll.mit.edu>
2022-09-01 07:01:08 -0400

    - Interim checkin trying to get multi-core working in verilator

--- sims/cep_cosim/testSuites/bareMetalTests/macroMix/riscv_wrapper.cc
+++ sims/cep_cosim/testSuites/bareMetalTests/macroMix/riscv_wrapper.cc
@@ -20,6 +20,10 @@
   #include "portable_io.h"
   #include "CEP.h"
   #include "cepRegTest.h"
+#ifdef VERILATOR
+  #include "smp.h"
+  #define smpResume() asm ("#smp_resume(s1, s2)")
+#endif
 
   // Include the test vectors related to this test
   #include "SROT_playback.h"
@@ -51,48 +55,62 @@
   void thread_entry(int cid, int nc) {
 #endif
     
+
     int errCnt    = 0;
     int testId[4] = {0x00, 0x11, 0x22, 0x33};
     int coreId    = read_csr(mhartid);
     int revCheck  = 1;
-    int verbose   = 0;
+    int verbose   = 1;
     int maxTO     = 5000;
     uint64_t upper;
     uint64_t lower;
-    
+
+#ifdef VERILATOR
+    if (coreId == 0)
+      smpResume();
+#endif
+
     // Set the current core's status to running
     set_cur_status(CEP_RUNNING_STATUS);
+
+    LOGI("Core %d initialized\n", coreId);
+
+    goto cleanup;
 
     // There is no Crypto++ support for RISC-V, so pre-recorded vectors will be used
     if (coreId == 0) {
       upper = SROT_adrBase + SROT_adrSize;
       lower = SROT_adrBase;
-      errCnt += cep_playback(SROT_playback, upper, lower, SROT_totalCommands, 0);
+      errCnt += cep_playback(SROT_playback, upper, lower, SROT_totalCommands, verbose);
       cep_write64(CEP_VERSION_REG_INDEX, cep_scratch4_reg, CEP_OK2RUN_SIGNATURE);
+      LOGI("Core %d running test\n", coreId);
       upper = AES_adrBase + AES_adrSize;
       lower = AES_adrBase;
-      errCnt += cep_playback(AES_playback, upper, lower, AES_totalCommands, 0);    
+      errCnt += cep_playback(AES_playback, upper, lower, AES_totalCommands, verbose);    
     }
     else if (coreId == 1) {
       errCnt += cep_readNspin(CEP_VERSION_REG_INDEX, cep_scratch4_reg, CEP_OK2RUN_SIGNATURE, 0xFFFFFFFF, maxTO); 
       if (errCnt) goto cleanup;
+      LOGI("Core %d running test\n", coreId);
       upper = SHA256_1_adrBase + SHA256_1_adrSize;
       lower = SHA256_1_adrBase;
-      errCnt += cep_playback(SHA256_1_playback, upper, lower, SHA256_1_totalCommands, 0);    
+      errCnt += cep_playback(SHA256_1_playback, upper, lower, SHA256_1_totalCommands, verbose);    
     }
     else if (coreId == 2) {
       errCnt += cep_readNspin(CEP_VERSION_REG_INDEX, cep_scratch4_reg, CEP_OK2RUN_SIGNATURE, 0xFFFFFFFF, maxTO); 
       if (errCnt) goto cleanup;
+      LOGI("Core %d running test\n", coreId);
       upper = IIR_adrBase + IIR_adrSize;
       lower = IIR_adrBase;
-      errCnt += cep_playback(IIR_playback, upper, lower, IIR_totalCommands, 0);    
+      errCnt += cep_playback(IIR_playback, upper, lower, IIR_totalCommands, verbose);    
     }
     else if (coreId == 3) {
       errCnt += cep_readNspin(CEP_VERSION_REG_INDEX, cep_scratch4_reg, CEP_OK2RUN_SIGNATURE, 0xFFFFFFFF, maxTO); 
       if (errCnt) goto cleanup;
+      LOGI("Core %d running test\n", coreId);
       upper = FIR_adrBase + FIR_adrSize;
       lower = FIR_adrBase;
-      errCnt += cep_playback(FIR_playback, upper, lower, FIR_totalCommands, 0);    
+      errCnt += cep_playback(FIR_playback, upper, lower, FIR_totalCommands, verbose);    
     }  
 
     // Set the core status
