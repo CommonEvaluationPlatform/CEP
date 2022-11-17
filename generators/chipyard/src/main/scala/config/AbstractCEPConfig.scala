f194d669eacc14cc948fcad20d3aedc492ea64b9
Brendon Chetwynd <brendon.chetwynd@ll.mit.edu>
2022-11-17 13:57:10 -0500

    - CEP builds, bare metal srotMemTest runs, version updated to v4.4

--- generators/chipyard/src/main/scala/config/AbstractCEPConfig.scala
+++ generators/chipyard/src/main/scala/config/AbstractCEPConfig.scala
@@ -3,7 +3,7 @@ package chipyard.config
 import freechips.rocketchip.config.{Config}
 import freechips.rocketchip.subsystem._
 
-// The following AbstractCEPConfig removes the L2 cache (when compared to AbstractConfig)
+// The following defines the abstract configuration for non-FPGA based CEP build targets
 class AbstractCEPConfig extends Config(
 
   // While the test harness is not used for the CEP, the following is required to complete the chipyard build
@@ -18,17 +18,18 @@ class AbstractCEPConfig extends Config(
   new chipyard.iobinders.WithDividerOnlyClockGenerator ++
   
   // Additional chip configuration items
-  new chipyard.config.WithNoSubsystemDrivenClocks ++              // drive the subsystem diplomatic clocks from ChipTop instead of using implicit clocks
-  new chipyard.config.WithInheritBusFrequencyAssignments ++       // Unspecified clocks within a bus will receive the bus frequency if set
-  new chipyard.config.WithPeripheryBusFrequencyAsDefault ++       // Unspecified frequencies with match the pbus frequency (which is always set)
-  new chipyard.config.WithMemoryBusFrequency(200.0) ++            // Default 200 MHz mbus
-  new chipyard.config.WithPeripheryBusFrequency(200.0) ++         // Default 200 MHz pbus
-  new freechips.rocketchip.subsystem.WithJtagDTM ++               // set the debug module to expose a JTAG port
-  new freechips.rocketchip.subsystem.WithNoMMIOPort ++            // no top-level MMIO master port (overrides default set in rocketchip)
-  new freechips.rocketchip.subsystem.WithNoSlavePort ++           // no top-level MMIO slave port (overrides default set in rocketchip)
-  new freechips.rocketchip.subsystem.WithNExtTopInterrupts(0) ++  // no external interrupts
-  new freechips.rocketchip.subsystem.WithCoherentBusTopology ++   // hierarchical buses including sbus/mbus/pbus/fbus/cbus/l2
-  new freechips.rocketchip.system.BaseConfig                      // "base" rocketchip system
+  new chipyard.config.WithNoSubsystemDrivenClocks ++                    // drive the subsystem diplomatic clocks from ChipTop instead of using implicit clocks
+  new chipyard.config.WithInheritBusFrequencyAssignments ++             // Unspecified clocks within a bus will receive the bus frequency if set
+  new chipyard.config.WithPeripheryBusFrequencyAsDefault ++             // Unspecified frequencies with match the pbus frequency (which is always set)
+  new chipyard.config.WithMemoryBusFrequency(200.0) ++                  // Default 200 MHz mbus
+  new chipyard.config.WithPeripheryBusFrequency(200.0) ++               // Default 200 MHz pbus
+  new freechips.rocketchip.subsystem.WithJtagDTM ++                     // set the debug module to expose a JTAG port
+  new freechips.rocketchip.subsystem.WithNoMMIOPort ++                  // no top-level MMIO master port (overrides default set in rocketchip)
+  new freechips.rocketchip.subsystem.WithNoSlavePort ++                 // no top-level MMIO slave port (overrides default set in rocketchip)
+  new freechips.rocketchip.subsystem.WithNExtTopInterrupts(0) ++        // no external interrupts
+  new freechips.rocketchip.subsystem.WithDontDriveBusClocksFromSBus ++  // leave the bus clocks undriven by sbus
+  new freechips.rocketchip.subsystem.WithCoherentBusTopology ++         // hierarchical buses including sbus/mbus/pbus/fbus/cbus/l2
+  new freechips.rocketchip.system.BaseConfig                            // "base" rocketchip system
 )
 
 // Tweak to the default Chipyard AbstractConfig that removes the L2 Cache
@@ -38,8 +39,8 @@ class AbstractNoL2Config extends Config(
   new chipyard.harness.WithBlackBoxSimMem ++                    // add SimDRAM DRAM model for axi4 backing memory, if axi4 mem is enabled
   new chipyard.harness.WithSimSerial ++                         // add external serial-adapter and RAM
   new chipyard.harness.WithSimDebug ++                          // add SimJTAG or SimDTM adapters if debug module is enabled
-  new chipyard.harness.WithSimSPIFlashModel ++
   new chipyard.harness.WithGPIOTiedOff ++                       // tie-off chiptop GPIOs, if GPIOs are present
+  new chipyard.harness.WithSimSPIFlashModel ++                  // add simulated SPI flash memory, if SPI is enabled
   new chipyard.harness.WithSimAXIMMIO ++                        // add SimAXIMem for axi4 mmio port, if enabled
   new chipyard.harness.WithTieOffInterrupts ++                  // tie-off interrupt ports, if present
   new chipyard.harness.WithTieOffL2FBusAXI ++                   // tie-off external AXI4 master, if present
@@ -57,26 +58,27 @@ class AbstractNoL2Config extends Config(
   new chipyard.iobinders.WithDebugIOCells ++
   new chipyard.iobinders.WithUARTIOCells ++
   new chipyard.iobinders.WithGPIOCells ++
-  new chipyard.iobinders.WithSPIIOCells ++
+  new chipyard.iobinders.WithSPIFlashIOCells ++
   new chipyard.iobinders.WithTraceIOPunchthrough ++
   new chipyard.iobinders.WithExtInterruptIOCells ++
   new chipyard.iobinders.WithCustomBootPin ++
   new chipyard.iobinders.WithDividerOnlyClockGenerator ++
 
-  new testchipip.WithSerialTLWidth(32) ++                        // fatten the serialTL interface to improve testing performance
-  new testchipip.WithDefaultSerialTL ++                          // use serialized tilelink port to external serialadapter/harnessRAM
-  new chipyard.config.WithBootROM ++                             // use default bootrom
-  new chipyard.config.WithUART ++                                // add a UART
-  new chipyard.config.WithNoSubsystemDrivenClocks ++             // drive the subsystem diplomatic clocks from ChipTop instead of using implicit clocks
-  new chipyard.config.WithInheritBusFrequencyAssignments ++      // Unspecified clocks within a bus will receive the bus frequency if set
-  new chipyard.config.WithPeripheryBusFrequencyAsDefault ++      // Unspecified frequencies with match the pbus frequency (which is always set)
-  new chipyard.config.WithMemoryBusFrequency(100.0) ++           // Default 100 MHz mbus
-  new chipyard.config.WithPeripheryBusFrequency(100.0) ++        // Default 100 MHz pbus
-  new freechips.rocketchip.subsystem.WithJtagDTM ++              // set the debug module to expose a JTAG port
-  new freechips.rocketchip.subsystem.WithNoMMIOPort ++           // no top-level MMIO master port (overrides default set in rocketchip)
-  new freechips.rocketchip.subsystem.WithNoSlavePort ++          // no top-level MMIO slave port (overrides default set in rocketchip)
-  new freechips.rocketchip.subsystem.WithNExtTopInterrupts(0) ++ // no external interrupts
-  new freechips.rocketchip.subsystem.WithCoherentBusTopology ++  // hierarchical buses including sbus/mbus/pbus/fbus/cbus/l2
-  new freechips.rocketchip.system.BaseConfig)                    // "base" rocketchip system
+  new testchipip.WithSerialTLWidth(32) ++                           // fatten the serialTL interface to improve testing performance
+  new testchipip.WithDefaultSerialTL ++                             // use serialized tilelink port to external serialadapter/harnessRAM
+  new chipyard.config.WithBootROM ++                                // use default bootrom
+  new chipyard.config.WithUART ++                                   // add a UART
+  new chipyard.config.WithNoSubsystemDrivenClocks ++                // drive the subsystem diplomatic clocks from ChipTop instead of using implicit clocks
+  new chipyard.config.WithInheritBusFrequencyAssignments ++         // Unspecified clocks within a bus will receive the bus frequency if set
+  new chipyard.config.WithPeripheryBusFrequencyAsDefault ++         // Unspecified frequencies with match the pbus frequency (which is always set)
+  new chipyard.config.WithMemoryBusFrequency(100.0) ++              // Default 100 MHz mbus
+  new chipyard.config.WithPeripheryBusFrequency(100.0) ++           // Default 100 MHz pbus
+  new freechips.rocketchip.subsystem.WithJtagDTM ++                 // set the debug module to expose a JTAG port
+  new freechips.rocketchip.subsystem.WithNoMMIOPort ++              // no top-level MMIO master port (overrides default set in rocketchip)
+  new freechips.rocketchip.subsystem.WithNoSlavePort ++             // no top-level MMIO slave port (overrides default set in rocketchip)
+  new freechips.rocketchip.subsystem.WithNExtTopInterrupts(0) ++    // no external interrupts
+  new freechips.rocketchip.subsystem.WithDontDriveBusClocksFromSBus ++ // leave the bus clocks undriven by sbus
+  new freechips.rocketchip.subsystem.WithCoherentBusTopology ++     // hierarchical buses including sbus/mbus/pbus/fbus/cbus/l2
+  new freechips.rocketchip.system.BaseConfig)                       // "base" rocketchip system
 
 
